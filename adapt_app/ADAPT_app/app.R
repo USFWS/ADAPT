81fd28a8fe711a96733a1299d35caab27bd2be00
beth-ross <beth_ross@fws.gov>
2023-12-08 13:31:15 -0700

    updates to layout and predictions

--- adapt_app/ADAPT_app/app.R
+++ adapt_app/ADAPT_app/app.R
@@ -1,52 +1,73 @@
 # Shiny app for At-risk species Distribution Analysis and Data T... Tool
 
 library(shiny)
-library(ggplot2)
+#library(ggplot2)
 library(tidyr)
 #library(INLA)
 #library(inlabru)
-library(flexdashboard)
+#library(flexdashboard)
 library(shinydashboard)
 library(leaflet)
-library(maps)
+#library(maps)
 library(sf)
-library(stars)
+#library(stars)
 library(scico)
 library(terra)
 library(tidyterra)
-library(lwgeom)
+#library(lwgeom)
 library(shinyWidgets)
+library(shinycssloaders)
 
-adapt_pts <- st_read("~/ADAPT/adapt_app/ADAPT_app/dat_short.shp")
+adapt_pts <- st_read("dat_short.shp")
 
 #read in raster files
-beth_rast_ssp2_2100 <- rast("~/ADAPT/adapt_app/ADAPT_app/beth_ssp2_2100.tif")
-#beth_rast_ssp5_2100 <- rast("preds_ssp5_2100.tif")
-beth_rast_current <- rast("~/ADAPT/adapt_app/ADAPT_app/beth_current.tif")
+beth_ssp2_2100 <- rast("beth_ssp2_2100.tif")
+beth_ssp2_2060 <- rast('beth_ssp2_2060.tif')
+beth_ssp5_2100 <- rast("beth_ssp5_2100.tif")
+beth_ssp5_2060 <- rast("beth_ssp5_2060.tif")
+beth_current <- rast("beth_current.tif")
 
-buow_current <- rast("~/ADAPT/adapt_app/ADAPT_app/buow_current.tif")
-buow_ssp2_2100 <- rast("~/ADAPT/adapt_app/ADAPT_app/buow_ssp2_2100.tif")
+buow_current <- rast("buow_current.tif")
+buow_ssp2_2060 <- rast("buow_ssp2_2060.tif")
+buow_ssp2_2100 <- rast("buow_ssp2_2100.tif")
+buow_ssp5_2060 <- rast("buow_ssp5_2060.tif")
+buow_ssp5_2100 <- rast("buow_ssp5_2100.tif")
 
-lewo_current <- rast("~/ADAPT/adapt_app/ADAPT_app/lewo_test_25kmesh_500kmatern.tif")
-lewo_ssp2_2100 <- rast("~/ADAPT/adapt_app/ADAPT_app/lewo_preds_ssp2_2100.tif")
+lewo_current <- rast("lewo_current.tif")
+lewo_ssp2_2060 <- rast("lewo_ssp2_2060.tif")
+lewo_ssp2_2100 <- rast("lewo_ssp2_2100.tif")
+lewo_ssp5_2060 <- rast("lewo_ssp5_2060.tif")
+lewo_ssp5_2100 <- rast("lewo_ssp5_2100.tif")
 
 #need function here to read in raster and make brick - 
  # or just need to upload bricks!
 
-beth_brick <- c(beth_rast_current, beth_rast_ssp2_2100,diff = beth_rast_ssp2_2100-beth_rast_current)
-buow_brick <- c(buow_current, buow_ssp2_2100,diff = buow_current-buow_ssp2_2100)
-lewo_brick <- c(lewo_current, lewo_ssp2_2100,diff = lewo_current-lewo_ssp2_2100)
+beth_brick <- list(current = beth_current, 
+                mid = c(beth_ssp2_2060,
+                        beth_ssp5_2060),
+                end = c(beth_ssp2_2100,
+                        beth_ssp5_2100)
+)
+
+buow_brick <- list(current = buow_current, 
+                   mid = c(buow_ssp2_2060,
+                           buow_ssp5_2060),
+                   end = c(buow_ssp2_2100,
+                           buow_ssp5_2100))
+
+lewo_brick <- list(current = lewo_current, 
+                   mid = c(lewo_ssp2_2060,
+                           lewo_ssp5_2060),
+                   end = c(lewo_ssp2_2100,
+                           lewo_ssp5_2100))
 
 rasters_to_plot <- list(beth = beth_brick,
                         buow = buow_brick,
                         lewo = lewo_brick)
 
-#rasters_to_plot[[4]] <- rasters_to_plot$SSP2-rasters_to_plot$current
-#names(rasters_to_plot)[4] <- "Diff_SSP2"
-#rasters_to_plot[[5]] <- rasters_to_plot$SSP5-rasters_to_plot$current
-#names(rasters_to_plot)[5] <- "Diff_SSP5"
-
-# Define UI for application that plots prediction rasters
+source("R/species_input.R")
+source("R/ssp_input.R")
+source("R/time_input.R")
 
 ui <- fluidPage(
   tabsetPanel(
@@ -55,65 +76,78 @@ ui <- fluidPage(
     titlePanel("Predictions of Occupancy Probability"),
 
     fluidRow(  
-       column(4,
-              selectInput("time",
-                          label = "Time Period",
-                          choices = c("Current" = 1,
-                                       "2100" = 2)),
+      column(
+        4,
+        species_input("species")
+      ),
+       
+      column(4,
+          time_input("time")
+             ),
               
-              conditionalPanel("input.time === 2",
-                        selectInput("ssp",label = "SSP",
-                                    choices = c("SSP2-4.5" = 2)),
-                        radioButtons("diff",label = "Difference of current and future",
-                                     c("Show future predictions" = 2 ,
-                                       "Show difference between current and future predictions" = 3)))
-              
-              #selectInput(inputId = "ssp", label = "SSP:",
-              #  choices = c("Current" = 1,
-               #            "SSP2-4.5" = 2,
-                 #           "Difference of current and SSP2" = 3
-                                 #"Difference of current and SSP5" = 5
-                   #         )
-                  #   )
-         )),
-    fluidRow(   
-      column(4,selectInput(
-        inputId = "species", 
-        label = "Species",
-        choices = c("Bendire's Thrasher"= 1,
-                        "Burrowing Owl" = 2,
-                   "Lewis's Woodpecker" = 3)))),
-         
-        # Show a plot of the generated distribution
+      column(4,
+             conditionalPanel(
+              condition = "input.time == 2 | input.time == 3",
+                ssp_input("ssp")
+              )
+                
+       )
+      ),
+  
+    fluidRow(
+      column(4, offset = 4,
+             checkboxInput(inputId = "compare",
+                  label = "Compare Scenarios",
+                  value = FALSE)),
+      
+      column(4, 
+      
+      conditionalPanel(
+        condition = "input.compare == true",
+        selectInput(input = "time_diff",
+                    label = "Time Period",
+                    choices = c("2040-2060" = 2,
+                                "2080-2100" = 3)),
+        ssp_input("ssp_diff")
+      )
+             )),
+    
+    # Show a plot of the generated distribution
       fluidRow(
-           column(12,leafletOutput(outputId = "ssp_plot")),
-           #column(4,plotOutput(outputId = "current_plot")),
-           #column(4,plotOutput(outputId = "difference_plot"))
+      column(12,
+             withSpinner(
+               leafletOutput(outputId = "ssp_plot"))),
         ),
+      
       fluidRow(
         column(4, downloadButton("download_raster","Download Data"))
       )
     ),
 
+#### Tab 2 ####
   tabPanel("Data Summary",
-           #### Tab 2 ####
+  titlePanel("Data Summary & Map"),         
+
     fluidRow(
-      column(6,
-        selectInput(inputId = "year",
+      column(4,
+         sliderInput("year",
                     label = "Year",
-                    choices = c("2017","2018","2019","2020","2021")),
-                    
-        selectInput(inputId = "state",
+                    min = 2017, max = 2021,
+                    value = c(2017,2021),
+                    sep = "")
+                    ),
+      
+      column(4,
+        pickerInput(inputId = "state",
                       label = "State",
                     choices = unique(adapt_pts$ID),
-                    #options = list(`actions-box` = TRUE),
-                    multiple = TRUE),
-        selectInput(inputId = "species_plot",
-                      label = "Species",
-                    choices = c("Bendire's Thrasher" = "beth",
-                                "Burrowing Owl" = "buow",
-                                "Lewis's Woodpecker" = "lewo"))
-               )),
+                    options = list(`actions-box` = TRUE),
+                    multiple = TRUE)
+        ),
+      column(4,
+        species_input("species_plot")
+        )
+      ),
              
     fluidRow(
          column(12, leafletOutput("map"))
@@ -132,23 +166,31 @@ server <- function(input, output, session) {
   #tab 1
    
   raster_subset <- reactive({
+    if(input$compare == TRUE){
+      if(input$time == 1){
+        rasters_to_plot[[as.numeric(input$species)]][[as.numeric(input$time_diff)]][[as.numeric(input$ssp_diff)]] - 
+          rasters_to_plot[[as.numeric(input$species)]][[1]]
+      } else {
+      rasters_to_plot[[as.numeric(input$species)]][[as.numeric(input$time_diff)]][[as.numeric(input$ssp_diff)]] - 
+        rasters_to_plot[[as.numeric(input$species)]][[as.numeric(input$time)]][[as.numeric(input$ssp)]]
+    }
+      } else {
+    
     if(input$time == 1) {
       rasters_to_plot[[as.numeric(input$species)]][[1]]  
     } else {
-      rasters_to_plot[[as.numeric(input$species)]][[input$diff]]
+      rasters_to_plot[[as.numeric(input$species)]][[as.numeric(input$time)]][[as.numeric(input$ssp)]]
     }
-  })
+  }})
   
   pal <- reactive({
     colorNumeric(
-    palette = ifelse(input$diff == 3,"broc", "tokyo"),
+      scico(9, alpha = 0.8, palette = "tokyo"), #ifelse(as.numeric(input$ssp) == 3,"broc", "tokyo")),
     values(raster_subset()),
     na.color = "transparent")
   })
   
   output$ssp_plot <- renderLeaflet({
-  # generate plot based on input$ssp from ui.R
-
     leaflet() |>  
       addTiles() |>  
       addRasterImage(
@@ -170,19 +212,15 @@ server <- function(input, output, session) {
     }
   )
   
-  #}
 
-# Run the application 
-#shinyApp(ui = ui, server = server)
+#tab 2
 
-  #tab 2
-#server_t2 <- function(input, output, session){
-  year_subset <- reactive({
-    adapt_pts |>  
-      filter(year == input$year) |>  
-      filter(ID == input$state) |> 
-      filter(species == input$species_plot)
-  })
+#  year_subset <- reactive({
+#    adapt_pts |>  
+#      filter(year == input$year) |>  
+#      filter(ID == input$state) |> 
+#      filter(species == unique(species)[as.numeric(input$species_plot)])
+#  })
   
   #output$year_summary <- renderTable(
   #  table(year_subset()$effort)
@@ -193,7 +231,7 @@ server <- function(input, output, session) {
     req(input$year)
     req(input$species_plot)
     adapt_pts |> 
-      filter(ID == c(input$state) & year == input$year & species == input$species_plot) |> 
+      filter(ID == c(input$state) & year == input$year & species == unique(species)[as.numeric(input$species_plot)]) |> 
       arrange(obs)
     })
   
