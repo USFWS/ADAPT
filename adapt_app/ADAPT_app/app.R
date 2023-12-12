f2a57e9a46ac257d5cf7bb68372f424110800882
beth-ross <beth_ross@fws.gov>
2023-12-12 14:21:42 -0700

    updated for better caching

--- adapt_app/ADAPT_app/app.R
+++ adapt_app/ADAPT_app/app.R
@@ -21,53 +21,57 @@ library(shinycssloaders)
 adapt_pts <- st_read("dat_short.shp")
 
 #read in raster files
-beth_ssp2_2100 <- rast("beth_ssp2_2100.tif")
-beth_ssp2_2060 <- rast('beth_ssp2_2060.tif')
-beth_ssp5_2100 <- rast("beth_ssp5_2100.tif")
-beth_ssp5_2060 <- rast("beth_ssp5_2060.tif")
-beth_current <- rast("beth_current.tif")
-
-buow_current <- rast("buow_current.tif")
-buow_ssp2_2060 <- rast("buow_ssp2_2060.tif")
-buow_ssp2_2100 <- rast("buow_ssp2_2100.tif")
-buow_ssp5_2060 <- rast("buow_ssp5_2060.tif")
-buow_ssp5_2100 <- rast("buow_ssp5_2100.tif")
-
-lewo_current <- rast("lewo_current.tif")
-lewo_ssp2_2060 <- rast("lewo_ssp2_2060.tif")
-lewo_ssp2_2100 <- rast("lewo_ssp2_2100.tif")
-lewo_ssp5_2060 <- rast("lewo_ssp5_2060.tif")
-lewo_ssp5_2100 <- rast("lewo_ssp5_2100.tif")
+# beth_ssp2_2100 <- rast("beth_ssp2_2100.tif")
+# beth_ssp2_2060 <- rast('beth_ssp2_2060.tif')
+# beth_ssp5_2100 <- rast("beth_ssp5_2100.tif")
+# beth_ssp5_2060 <- rast("beth_ssp5_2060.tif")
+# beth_current <- rast("beth_current.tif")
+# 
+# buow_current <- rast("buow_current.tif")
+# buow_ssp2_2060 <- rast("buow_ssp2_2060.tif")
+# buow_ssp2_2100 <- rast("buow_ssp2_2100.tif")
+# buow_ssp5_2060 <- rast("buow_ssp5_2060.tif")
+# buow_ssp5_2100 <- rast("buow_ssp5_2100.tif")
+# 
+# lewo_current <- rast("lewo_current.tif")
+# lewo_ssp2_2060 <- rast("lewo_ssp2_2060.tif")
+# lewo_ssp2_2100 <- rast("lewo_ssp2_2100.tif")
+# lewo_ssp5_2060 <- rast("lewo_ssp5_2060.tif")
+# lewo_ssp5_2100 <- rast("lewo_ssp5_2100.tif")
 
 #need function here to read in raster and make brick - 
  # or just need to upload bricks!
 
-beth_brick <- list(current = beth_current, 
-                mid = c(beth_ssp2_2060,
-                        beth_ssp5_2060),
-                end = c(beth_ssp2_2100,
-                        beth_ssp5_2100)
-)
+# beth_brick <- c(beth_current, 
+#                 beth_ssp2_2060,
+#                 beth_ssp5_2060,
+#                 beth_ssp2_2100,
+#                 beth_ssp5_2100)
+# 
+# writeRaster(beth_brick,"beth_brick.tif",overwrite=TRUE)
+# 
+# buow_brick <- list(current = buow_current, 
+#                    mid = c(buow_ssp2_2060,
+#                            buow_ssp5_2060),
+#                    end = c(buow_ssp2_2100,
+#                            buow_ssp5_2100))
+# 
+# lewo_brick <- list(current = lewo_current, 
+#                    mid = c(lewo_ssp2_2060,
+#                            lewo_ssp5_2060),
+#                    end = c(lewo_ssp2_2100,
+#                            lewo_ssp5_2100))
 
-buow_brick <- list(current = buow_current, 
-                   mid = c(buow_ssp2_2060,
-                           buow_ssp5_2060),
-                   end = c(buow_ssp2_2100,
-                           buow_ssp5_2100))
+beth_brick <- rast("beth_brick.tif")
+buow_brick <- rast("buow_brick.tif")
+lewo_brick <- rast("lewo_brick.tif")
 
-lewo_brick <- list(current = lewo_current, 
-                   mid = c(lewo_ssp2_2060,
-                           lewo_ssp5_2060),
-                   end = c(lewo_ssp2_2100,
-                           lewo_ssp5_2100))
-
-rasters_to_plot <- list(beth = beth_brick,
-                        buow = buow_brick,
-                        lewo = lewo_brick)
+rasters_to_plot <- list(beth_brick,buow_brick,
+                        lewo_brick)
 
 source("R/species_input.R")
-source("R/ssp_input.R")
-source("R/time_input.R")
+#source("R/ssp_input.R")
+#source("R/time_input.R")
 
 ui <- fluidPage(
   tabsetPanel(
@@ -82,35 +86,51 @@ ui <- fluidPage(
       ),
        
       column(4,
-          time_input("time")
+          selectizeInput("time","Scenario",
+                         choices = list(
+                           Current = list(`Current` = 1),
+                           SSP2 = list(`2040-2060` = 2,
+                                       `2080-2100` = 4),
+                           SSP5 = list(`2040-2060` = 3,
+                                       `2080-2100` = 5),
+                           Comparison = list(
+                             `SSP2 at 2060 vs 2100` = 6,
+                             `SSP2 at current vs 2060` = 8,
+                             `SSP2 at current vs 2100` = 7,
+                             `SSP5 at 2060 vs 2100` = 9,
+                             `SSP5 at current vs 2060` = 11,
+                             `SSP5 at current vs 2100` = 10,
+                             `SSP2 vs SSP5 at 2060` = 13,
+                             `SSP2 vs SSP5 at 2100` = 12)
+                         ))
              ),
               
-      column(4,
-             conditionalPanel(
-              condition = "input.time == 2 | input.time == 3",
-                ssp_input("ssp")
-              )
-                
-       )
+      # column(4,
+      #        conditionalPanel(
+      #         condition = "input.time == 2 | input.time == 3",
+      #           ssp_input("ssp")
+      #         )
+      #           
+      #  )
       ),
   
-    fluidRow(
-      column(4, offset = 4,
-             checkboxInput(inputId = "compare",
-                  label = "Compare Scenarios",
-                  value = FALSE)),
+    # fluidRow(
+    #   column(4, offset = 4,
+    #          checkboxInput(inputId = "compare",
+    #               label = "Compare Scenarios",
+    #               value = FALSE)),
       
-      column(4, 
-      
-      conditionalPanel(
-        condition = "input.compare == true",
-        selectInput(input = "time_diff",
-                    label = "Time Period",
-                    choices = c("2040-2060" = 2,
-                                "2080-2100" = 3)),
-        ssp_input("ssp_diff")
-      )
-             )),
+      # column(4, 
+      # 
+      # conditionalPanel(
+      #   condition = "input.compare == true",
+      #   selectInput(input = "time_diff",
+      #               label = "Time Period",
+      #               choices = c("2040-2060" = 2060,
+      #                           "2080-2100" = 2100)),
+      #   ssp_input("ssp_diff")
+      # )
+      #        )),
     
     # Show a plot of the generated distribution
       fluidRow(
@@ -166,26 +186,30 @@ server <- function(input, output, session) {
   #tab 1
    
   raster_subset <- reactive({
-    if(input$compare == TRUE){
-      if(input$time == 1){
-        rasters_to_plot[[as.numeric(input$species)]][[as.numeric(input$time_diff)]][[as.numeric(input$ssp_diff)]] - 
-          rasters_to_plot[[as.numeric(input$species)]][[1]]
-      } else {
-      rasters_to_plot[[as.numeric(input$species)]][[as.numeric(input$time_diff)]][[as.numeric(input$ssp_diff)]] - 
-        rasters_to_plot[[as.numeric(input$species)]][[as.numeric(input$time)]][[as.numeric(input$ssp)]]
-    }
-      } else {
-    
-    if(input$time == 1) {
-      rasters_to_plot[[as.numeric(input$species)]][[1]]  
-    } else {
-      rasters_to_plot[[as.numeric(input$species)]][[as.numeric(input$time)]][[as.numeric(input$ssp)]]
-    }
-  }})
+    rasters_to_plot[[as.numeric(input$species)]][[as.numeric(input$time)]]
+    })
   
+  # raster_subset <- reactive({
+  #   if(input$compare == TRUE){
+  #     if(input$time == 1){
+  #       rasters_to_plot[[as.numeric(input$species)]][[as.numeric(input$time_diff)]][[as.numeric(input$ssp_diff)]] - 
+  #         rasters_to_plot[[as.numeric(input$species)]][[1]]
+  #     } else {
+  #     rasters_to_plot[[as.numeric(input$species)]][[as.numeric(input$time_diff)]][[as.numeric(input$ssp_diff)]] - 
+  #       rasters_to_plot[[as.numeric(input$species)]][[as.numeric(input$time)]][[as.numeric(input$ssp)]]
+  #   }
+  #     } else {
+  #   
+  #   if(input$time == 1) {
+  #     rasters_to_plot[[as.numeric(input$species)]][[1]]  
+  #   } else {
+  #     rasters_to_plot[[as.numeric(input$species)]][[as.numeric(input$time)]][[as.numeric(input$ssp)]]
+  #   }
+  # }})
+  # 
   pal <- reactive({
     colorNumeric(
-      scico(9, alpha = 0.8, palette = "tokyo"), #ifelse(as.numeric(input$ssp) == 3,"broc", "tokyo")),
+      scico(9, alpha = 0.8, palette = ifelse(as.numeric(input$time) > 5,"broc", "tokyo")),
     values(raster_subset()),
     na.color = "transparent")
   })
@@ -201,14 +225,15 @@ server <- function(input, output, session) {
         pal = pal(),
         values = values(raster_subset()),
         title = "Probability of Occupancy")
-  })
+  }) |> 
+    bindCache(input$species, input$time)
   
   output$download_raster <- downloadHandler(
     filename = function() {
-      paste0("output_preds_",input$ssp,"2100.tif")
+      paste0("output_preds_",input$species,".tif")
     },
     content = function(file) {
-      writeRaster(rasters_to_plot[[as.numeric(input$species)]][[as.numeric(input$ssp)]], file)
+      writeRaster(rasters_to_plot[[as.numeric(input$species)]][[as.numeric(input$time)]], file)
     }
   )
   
